2dcaec234fe6ac231e0d5e16e33e0af4d1f0a3f6
gonzalo <gonzalopulleirojuncal@gmail.com>
2024-09-25 12:43:53 +0200

    commit

---
+++ ToDo/src/main/java/com/gonzalo/proyect/Proyect.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gonzalo.proyect;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class Proyect {
+
+    public static void main(String[] args) {
+     
+        int a = 6;
+        int b = 5;
+        int c = 9;
+        
+        double media = ((a + b + c) / 3.0);
+        
+        System.out.println(media);
+    }
+   
+}
